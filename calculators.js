42a13293098be33a32bbee293487de237ca4a820
Tabang97 <tabangduda97@gmail.com>
2020-11-20 16:35:39 +0200

    cancel function left

---
+++ calculators.js
@@ -0,0 +1,17 @@
+// write numbers function
+function view(val){
+    document.getElementById('screen').value += val
+}
+
+function equal(){
+    let input = document.getElementById('screen').value
+    let res = eval(input)
+    document.getElementById("screen").value = res
+    // console.log(res)
+}
+
+function clear(){
+    document.getElementById('screen').value = ''
+
+    console.log("clear clicked")
+}
